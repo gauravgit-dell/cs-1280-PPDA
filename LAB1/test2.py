e2e853c50011b75e344309511f0687816594e0e7
gauravgit-dell <2REACHGPR@GMAIL.COM>
2025-01-27 15:29:44 +0530

    Add files via upload

---
+++ LAB1/test2.py
@@ -0,0 +1,11 @@
+G = int(input("Enter your marks"))
+if G>89:
+    print("A")
+elif G>79<G<90:
+    print("B")
+elif G>69<G<80:
+    print("C")
+elif G<70:
+    print("F")
+
+
---
+++ LAB1/test4.py
@@ -0,0 +1,5 @@
+n = int(input("enter a number"))
+i=2
+while i<=10:
+    print(f"{n}*{i}={n*i}")
+    i+=1
