78b01cc1be28451be7cf6c220fd83eb5547e0334
gauravgit-dell <2REACHGPR@GMAIL.COM>
2025-01-27 14:44:09 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+x=5
+if x==5:
+    print("number is 5")
+    
+x=int(input("Give some number "))
+if x>0:
+    print("it is a positive number")
+if x<0:
+    print("it is a negative number")
+elif x==0:
+    print("number is zero")
+    
+
+
