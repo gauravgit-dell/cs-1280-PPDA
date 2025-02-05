544364fe1e670783411b2de4ab791d38ac05bd72
gauravgit-dell <2REACHGPR@GMAIL.COM>
2025-02-05 15:34:39 +0530

    Update lists_lab3.py

--- LAB3/lists_lab3.py
+++ LAB3/lists_lab3.py
@@ -1,8 +1,10 @@
+#program1
 str = "hello"
 print(str)
 str2 = "gaurav"
 print(str2)
 
+#program2
 x = "python programming"
 print(x[:])
 print(x[0:])
@@ -11,13 +13,15 @@ print(x[::-1])
 print(x[-5:-3])
 print(x[-6:-4])
 
+#program3
 names = ["Alice", "Bob", "Charlie"]
 ages = [25, 30, 35]
 for index, name in enumerate(names):
     print(f"Index: {index}, Name: {name}")
 for name, age in zip(names, ages):
     print(f"{name} is {age} years old")
-    
+
+#program4
 import copy
 list1 = [1, [2, 3], 4]
 list2 = list1.copy() 
@@ -28,6 +32,7 @@ print("list1:", list1)
 print("list2:", list2) 
 print("list3:", list3) 
 
+#program5
 sum = 0
 newlist = []
 size = int(input('Enter size of the list: '))
@@ -38,14 +43,16 @@ for x in newlist:
     sum += x
 print('Sum =', sum)
 
+#program6
 squares = [i * i for i in range(10)]
 print(squares)
 
+#program7
 cubes = []
 for i in range(1, 11):
     cubes.append(i ** 3)
 print(cubes)
 
-
+#program8
 nums=[i for i in range(1, 21)]
 print(nums)
