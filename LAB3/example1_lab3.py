31f32b3060c1de50433726d3135526f95d110808
gauravgit-dell <2REACHGPR@GMAIL.COM>
2025-02-05 15:32:48 +0530

    Add files via upload
    all examples are in the same file

---
+++ LAB3/example1_lab3.py
@@ -0,0 +1,29 @@
+text=input("enter a string ")
+for char in text:
+    print(char,end=" " )
+
+text = input("Enter a string: ")
+def is_palindrome(string):
+    left, right = 0, len(string) - 1
+    while right >= left:
+        if string[left] != string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+print(is_palindrome(text))
+
+import time, sys
+start = time.time()
+squares = [x ** 2 for x in range(1,1000001)]
+end = time.time()
+print("List comprehension time:",end - start)
+print(sys.getsizeof(squares)) 
+
+start = time.time()
+squares = []
+for x in range(1, 1000001):
+    squares.append(x ** 2)
+end = time.time()
+print("For loop time:", end -start)
+print(sys.getsizeof(squares))
